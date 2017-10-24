b2ac49adb7d7607c540f171879c3bb5cb898eb7a
estasney <estasney@users.noreply.github.com>
2017-10-23 21:20:27 -0400

    class

--- utils/avature.py
+++ utils/avature.py
@@ -13,35 +13,52 @@ import os
 class DupDriver(object):
     def __init__(self, driver_path):
         self.driver_path = driver_path
-        self.lead_holder = []
-        self.avature_driver = webdriver.Chrome(executable_path=self.driver_path)
-        self.avature_driver.implicitly_wait(15)
+        self.dup_data_holder = {}
+        self.dup_count = 0
+
+
+    def setup_driver(self):
+        self.driver = webdriver.Chrome(executable_path=self.driver_path)
+        self.driver.implicitly_wait(15)
 
     def login_avature(self):
-        self.avature_driver.get("https://cisco.avature.net")
+        self.driver.get("https://cisco.avature.net")
         easygui.msgbox("Please Login Into Avature. Minimize this window and then click OK when you are logged in.")
         time.sleep(5)
         await_avature_login = easygui.ccbox("Confirming You Are Logged In to Avature")
-        if await_avature_login:
-            pass
-
-        self.avature_driver.get(
+        self.driver.get(
             "https://cisco.avature.net/#People/Id:2266/Filters:{\"entityTypeId\":2,\"id\":396094,\"set\":null,\"timeZone\":\"America*/New_York\"}")
         easygui.msgbox(
-            "DupCheck Will Now Check for " + str(len(self.lead_holder)) + " leads. You may minimize the browser "
+            "DupCheck Will Now Check for " + str(len(self.dup_data_holder)) + " leads. You may minimize the browser "
                                                                             "window. Please do not login to "
                                                                             "Avature through a different browser "
                                                                             "as this will stop the checker. Click "
                                                                             "OK when you are ready")
+    def tally_dup(self):
+        self.dup_count = self.dup_count + 1
 
-    def retrieve_key(self):
 
-    def dup_check_avature(dupcheck_key):
-        add_filter_menu = driver.find_element_by_xpath("//div[3]/div/span/span")
-        add_filter_menu_hover = ActionChains(driver).move_to_element(add_filter_menu)
-        add_filter_menu_hover.perform()
+    def present_dup(self): # extract one data record to check for duplicates
+        loaded_dup = self.dup_data_holder[self.dup_count]
+        return loaded_dup
+
+    def cursor_to_element(self, element):
+        element_to_hover = ActionChains(self.driver).move_to_element(element)
+        element_to_hover.perform()
+
+    def open_filter_dropdown(self):
+        add_filter_menu = self.driver.find_element_by_xpath("//div[3]/div/span/span")
+        self.cursor_to_element(add_filter_menu)
         add_filter_menu.click()
 
+    def set_filter_keywords(self):
+        try:
+            self.driver.find_element_by_xpath("//span[text()='Keywords']")
+        except NoSuch
+
+
+    def dup_check_avature(dupcheck_key):
+        self.open_filter_dropdown()
         keyword_filter = driver.find_element_by_css_selector(
             "div.FloatingMenuButton:nth-child(7) > span:nth-child(1) > span:nth-child(2)")
         keyword_filter.click()
@@ -81,10 +98,11 @@ class LeadPerson(object):
     def __init__(self, *key_data, **person_data):
         self.__dict__.update(person_data)
         self.__dict__['dup_key'] = self.dup_key(key_data)
-        self.prep_router(key_data)
 
-    def prep_router(self, *key_data):
-        pass
+    """
+    :key_data - Specify which dictionary keys from person_data are to be used for key generation
+    :person_data - Dictionary with desired person data
+    """
 
     def dup_key(self, *key_data):
         dup_key = []
@@ -95,6 +113,8 @@ class LeadPerson(object):
                     dup_key.append(linkedin_v)
                 else:
                     dup_key.append(k)
+        concated_key = self.concat_key(dup_key)
+        return concated_key
 
 
     def linkedin_key(self, url):
@@ -109,21 +129,8 @@ class LeadPerson(object):
         return url
 
 
-
-    def key_formats(self, dup_key_raw):
-        dup_key = []
-        for dk in dup_key_raw:
-            if "/in/" in dk:
-                dup_key.append(dk.split("\in\"))
-
-        if self.email:
-            dup_key.append(self.email)
-        if self.oemail:
-            dup_key.append(self.oemail)
-        if self.phone:
-            dup_key.append(self.phone)
-
-        dup_key = ' OR '.join([key for key in dup_key])
+    def concat_key(self, dup_key_raw):
+        dup_key = ' OR '.join([key for key in dup_key_raw])
         return dup_key
 
 
